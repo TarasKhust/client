864adf228b3e1b7e8ee32ef7253dad6bc37ca30f
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-02-14 18:07:52 +0200

    [CF-18][master] - add initial structure and add webpack

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,23 +6,15 @@ import {
 	Route,
 } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { store } from "../store";
-import Button from '../components/Button'
-import Users from 'container/User/Users'
+import { store } from "store";
+import Contact from 'container/Contact/Contact'
 
 export const history = createBrowserHistory();
 
 export default () => (
 	<Provider store={store}>
 		<Router history={history}>
-			<Switch>
-			<Route path={"/member/"}>
-				<Button/>
-			</Route>
-			<Route path={"/user/"}>
-				<Users/>
-			</Route>
-			</Switch>
+			<Contact/>
 		</Router>
 	</Provider>
 );
