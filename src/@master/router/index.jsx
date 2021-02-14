4a664219be536612ed407e35d5b3537af973e9ed
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-02-14 19:41:35 +0200

    [CF-16][master] - add docs

--- src/@master/router/index.jsx
+++ src/@master/router/index.jsx
@@ -4,13 +4,22 @@ import { Router } from "react-router-dom";
 import { createBrowserHistory } from 'history'
 import Contact from 'containers/Contact/Contact'
 import { store } from 'store'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 export const history = createBrowserHistory();
 
+const client = new ApolloClient({
+	uri: "https://48p1r2roz4.sse.codesandbox.io",
+	cache: new InMemoryCache()
+});
+
+
 export default () => (
-	<Provider store={store}>
-		<Router history={history}>
-			<Contact/>
-		</Router>
-	</Provider>
+		<ApolloProvider client={client}>
+			<Provider store={store}>
+				<Router history={history}>
+					<Contact/>
+				</Router>
+			</Provider>
+		</ApolloProvider>
 );
--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 
 const Contact = () => {
+
+	const { loading, error, data } = useQuery(gql`
+      {
+          rates(currency: "USD") {
+              currency
+              rate
+          }
+      }
+	`);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
 	return (
 			<div>
-				<h1>name</h1>
+				{data.rates.map(({ currency, rate }) => (
+						<div key={currency}>
+							<p>
+								{currency}: {rate}
+							</p>
+						</div>))}
 			</div>
 	)
 }
