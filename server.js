56b58871248e0cf87bdef6252bb0320c95793cce
Taras Rushchak | Dreamscape Networks <Taras.Rushchak@dreamscapenetworks.com>
2021-04-03 00:36:26 +0300

    FE-88 - create pipelines bitbucket-pipelines.yml

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('dist/index.html'));
+app.use(express.static('./dist/index.html'));
 app.set('port', process.env.PORT || 8080);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 })
 
 const server = app.listen(app.get('port'), function() {
